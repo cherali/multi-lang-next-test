d032f86f03848f1d8afd9bd47f454b374ae00d8b
alich6 <alicheraghi148@gmail.com>
2023-02-06 12:20:53 +0330

    homepage text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { FormattedMessage } from 'react-intl'
+
 export default function Home() {
-	return <div>hello world</div>
+	return (
+		<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+			<p className='text-4xl mt-2'>
+				<FormattedMessage id='app.header.home' defaultMessage='' />
+			</p>
+		</div>
+	)
 }
